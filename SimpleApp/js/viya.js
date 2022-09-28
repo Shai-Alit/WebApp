8146f605e56324bb0424f829756a40289d3f767b
Sean F <Sean.Ford@sas.com>
2022-09-28 08:40:52 -0500

    debug

--- SimpleApp/js/viya.js
+++ SimpleApp/js/viya.js
@@ -1,10 +1,15 @@
+let restaf = require('restaf');
 let store = restaf.initStore({casProxy: true});
-console.log(store);
 var currentSession;
 var viyahost = window.location.origin;
 var logged_user;
 
 var session = null ;
+
+let onClickLog     = null;
+let onClickListing = null;
+let onClickODS     = null;
+let currentJob     = null;
 
 /**
 *
@@ -48,5 +53,30 @@ function handleError(err){
 
 }
 
- console.log("session details" + appInit());
- console.log(logged_user)
+async function mainLoop (store, compute, code) {
+
+    // Define the payload
+    // This is your SAS program
+    let payload = {
+            data: {code: code}
+    };
+
+    // Execute the data step and wait for completion
+    // All calls to the server are managed by the store you created in step 1
+    let job    = await store.apiCall(session.links('execute'), payload);
+    let status = await store.jobState(job, null, 5, 2);
+
+    // Check the final status of job and view the logs
+    if (status.data === 'running') {
+        throw `ERROR: Job did not complete in allotted time`;
+    } else {
+        let logLines = await store.apiCall(status.job.links('log'));
+        // print the log
+        logViewer(logLines);
+    }
+    return 'restAF is cool or what';
+}
+let code =  [`data _null_; do i = 1 to 100; x=1; end; run; `];
+appInit()
+    .then(compute =>mainLoop(store,compute,code))
+    .catch(err => handleError(err));
