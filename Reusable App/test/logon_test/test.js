09f32f42a0aadd5a471831f6dbdcfc96be233459
Sean F <Sean.Ford@sas.com>
2022-10-21 12:49:08 -0500

    fixing test

--- Reusable App/test/logon_test/test.js
+++ Reusable App/test/logon_test/test.js
@@ -7,9 +7,16 @@ Entry point for test
 
 function run_test(){
     
-    
+    let logged_user = 'Not Logged In';
 	logonServer(window.location.origin).then ( msg => {
-		let logged_user = getUserIdentity();
+		getUserIdentity().then(rv => {
+			if (rv === 'None') {
+				logged_user = 'Error logging in';
+			}
+			else{
+				logged_user = rv;
+			}
+			});
 		setStatusMessage(msg + ' - user ID=' + logged_user,'info');
 	}).catch( err => handleError(err));
     
